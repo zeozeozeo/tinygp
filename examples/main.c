ca63c40ce32372e4635bdaa38961c0e7d4a7a7a4
zeozeozeo <zeocat9@gmail.com>
2023-03-12 16:02:33 +0500

    add some fun :D

--- examples/main.c
+++ examples/main.c
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <SDL_audio.h>
 #include <SDL_opengles2.h>
 #include <stdio.h>
 
@@ -6,6 +7,22 @@
 #define TGPGL_IMPLEMENTATION
 #include "tinygp.h"
 #include "tinygp_gl.h"
+
+#define AUDIO
+#ifdef AUDIO
+uint64_t t = 0;
+
+void audio_callback(void* userdata, uint8_t* stream, int len) {
+    // https://www.pouet.net/topic.php?which=8357&page=8#c388898
+    for (int i = 0; i < len; i++) {
+        stream[i] = ((t >> 1) * (15 & 0x234568a0 >> (t >> 8 & 28)) |
+                     t >> 1 >> (t >> 11) ^ t >> 12) +
+                    (t >> 4 & t & 24);
+        stream[i] /= 32;
+        t++;
+    }
+}
+#endif
 
 void draw(tgp_context* ctx, int width, int height) {
     tgp_begin(ctx, width, height);
@@ -16,10 +33,23 @@ void draw(tgp_context* ctx, int width, int height) {
 
 int main(void) {
     // setup SDL
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
         fprintf(stderr, "failed to initialize SDL: %s\n", SDL_GetError());
         exit(-1);
     }
+
+#ifdef AUDIO
+    // setup audio
+    SDL_AudioSpec     want = {}, have;
+    SDL_AudioDeviceID dev;
+    want.freq = 44100;
+    want.format = AUDIO_U8;
+    want.channels = 1;
+    want.samples = 4096;
+    want.callback = audio_callback;
+    dev = SDL_OpenAudioDevice(NULL, 0, &want, &have,
+                              SDL_AUDIO_ALLOW_SAMPLES_CHANGE);
+#endif
 
     // enable native IME
 #ifdef SDL_HINT_IME_SHOW_UI
@@ -49,6 +79,11 @@ int main(void) {
     tgpgl_context tgpgl_ctx;
     tgpgl_init_context(&tgpgl_ctx, &ctx);
 
+#ifdef AUDIO
+    // unpause the audio device
+    SDL_PauseAudioDevice(dev, 0);
+#endif
+
     bool running = true;
     while (running) {
         SDL_Event event;
@@ -76,9 +111,12 @@ int main(void) {
         SDL_GL_SwapWindow(window);
     }
 
-    // cleanup
-    // tgp_destroy_context(&ctx);
-    // tgpgl_destroy_context(&tgpgl_ctx);
+// cleanup
+// tgp_destroy_context(&ctx);
+// tgpgl_destroy_context(&tgpgl_ctx);
+#ifdef AUDIO
+    SDL_CloseAudioDevice(dev);
+#endif
     SDL_GL_DeleteContext(glc);
     SDL_DestroyWindow(window);
     SDL_Quit();
